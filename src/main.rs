63a4b6f5ce603631f780ddcb53d97223f4ab781e
Han Bender <jhibender@gmail.com>
2022-08-29 17:57:26 +0200

    yes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
-use std::thread;
-use std::time::Duration;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 fn main() {
-    call_me_baby();
-}
-
-fn call_me_baby() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("Hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    for i in 1..5 {
-        println!("Hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+    
 }
