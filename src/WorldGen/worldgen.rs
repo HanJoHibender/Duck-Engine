9b97b02fa8dc4b1378001cd504a1fe1a474f0eff
Han Bender <jhibender@gmail.com>
2022-08-29 17:57:26 +0200

    delete libraries folder, use builtins instead
    Rebase bender into main

---
+++ src/WorldGen/worldgen.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate matrix;
+
+use matrix::prelude::*;
+
+let mut sparse = Compressed::zero((2, 4));
+sparse.set((0, 0), 42.0);
+sparse.set((1, 3), 69.0);
+
+let dense = Conventional::from(&sparse);
+assert!(
+    &*dense == &*matrix![
+        42.0, 0.0, 0.0,  0.0;
+         0.0, 0.0, 0.0, 69.0;
+    ]
+);
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+fn main() {
+    
+}
