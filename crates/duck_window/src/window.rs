ed6f0ef8ec1eaa63c7ab886ea3845fc519523be2
Han Bender <jhibender@gmail.com>
2022-08-29 21:36:40 +0200

    rewritten the basic structure

---
+++ crates/duck_window/src/window.rs
@@ -0,0 +1,150 @@
+use std::sync::Arc;
+use std::task::Context;
+use vulkano::pipeline::Pipeline;
+use vulkano::swapchain::Surface;
+use vulkano::{
+    buffer::{BufferUsage, CpuAccessibleBuffer, TypedBufferAccess},
+    command_buffer::{
+        AutoCommandBufferBuilder, CommandBufferUsage, RenderPassBeginInfo, SubpassContents,
+    },
+    device::{
+        physical::{PhysicalDevice, PhysicalDeviceType},
+        Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo,
+    },
+    image::{view::ImageView, ImageAccess, ImageUsage, SwapchainImage},
+    impl_vertex,
+    instance::{Instance, InstanceCreateInfo},
+    pipeline::{
+        graphics::{
+            input_assembly::InputAssemblyState,
+            vertex_input::BuffersDefinition,
+            viewport::{Viewport, ViewportState},
+        },
+        GraphicsPipeline,
+    },
+    render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass},
+    swapchain::{
+        acquire_next_image, AcquireError, Swapchain, SwapchainCreateInfo, SwapchainCreationError,
+    },
+    sync::{self, FlushError, GpuFuture},
+};
+use vulkano_win::VkSurfaceBuild;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+pub struct duck_window {
+    pub title: &'static str,
+    pub width: i32,
+    pub height: i32,
+}
+
+impl duck_window {
+    pub fn new(title: &'static str, width: i32, height: i32) -> duck_window {
+        Self {
+            title,
+            width,
+            height,
+        }
+    }
+    pub fn init(&mut self) {
+        let required_extensions = vulkano_win::required_extensions();
+
+        let instance = Instance::new(InstanceCreateInfo {
+            enabled_extensions: required_extensions,
+
+            enumerate_portability: true,
+            ..Default::default()
+        })
+        .unwrap();
+
+        let event_loop = EventLoop::new();
+        let surface = WindowBuilder::new()
+            .build_vk_surface(&event_loop, instance.clone())
+            .unwrap();
+
+        let device_extensions = DeviceExtensions {
+            khr_swapchain: true,
+            ..DeviceExtensions::none()
+        };
+
+        let (physical_device, queue_family) = PhysicalDevice::enumerate(&instance)
+            .filter(|&p| p.supported_extensions().is_superset_of(&device_extensions))
+            .filter_map(|p| {
+                p.queue_families()
+                    .find(|&q| {
+                        q.supports_graphics() && q.supports_surface(&surface).unwrap_or(false)
+                    })
+                    .map(|q| (p, q))
+            })
+            .min_by_key(|(p, _)| match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+            })
+            .expect("No suitable physical device found");
+
+        let (device, mut queues) = Device::new(
+            physical_device,
+            DeviceCreateInfo {
+                enabled_extensions: device_extensions,
+                queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
+
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        let queue = queues.next().unwrap();
+
+        let (mut swapchain, images) = {
+            let surface_capabilities = physical_device
+                .surface_capabilities(&surface, Default::default())
+                .unwrap();
+
+            let image_format = Some(
+                physical_device
+                    .surface_formats(&surface, Default::default())
+                    .unwrap()[0]
+                    .0,
+            );
+
+            Swapchain::new(
+                device.clone(),
+                surface.clone(),
+                SwapchainCreateInfo {
+                    min_image_count: surface_capabilities.min_image_count,
+
+                    image_format,
+                    image_extent: surface.window().inner_size().into(),
+
+                    image_usage: ImageUsage::color_attachment(),
+
+                    composite_alpha: surface_capabilities
+                        .supported_composite_alpha
+                        .iter()
+                        .next()
+                        .unwrap(),
+
+                    ..Default::default()
+                },
+            )
+            .unwrap()
+        };
+
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            _ => {}
+        });
+    }
+    pub fn update(&self) {}
+}
