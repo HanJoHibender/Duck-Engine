e767111de8cdd5182c8feb8025f5706757fcac3f
Han Bender <jhibender@gmail.com>
2022-08-31 20:22:11 +0200

    skeleton of user input initiated, not done

--- examples/bender_test.rs
+++ examples/bender_test.rs
@@ -1,9 +1,18 @@
-use duck_internal::bender_window::window::simple_window;
+// use duck_internal::bender_window::window::simple_window;
+use duck_internal::bender_window::window::window_types::{
+    get_event_loop, get_window, simple_proxy_listener, UserEvent,
+};
 
+// use duck_internal::bender_window::window::simple_window::event_loop_proxy;
+// use duck_internal::duck_input::input::event_handler;
 fn main() {
     // spawn window
-    simple_window();
+    let event_loop = get_event_loop();
+    // let window =
+    get_window("Duck Engine!", event_loop);
 
+    // event_handler(event, window);
+    // simple_proxy_listener(Event<UserEvent>, event_loop);
     // innitialise render
     // window.insert_render();
     // generate World
